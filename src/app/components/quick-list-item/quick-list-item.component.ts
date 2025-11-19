4dab52d472e8bd12fb3ea60a4ef526a5bf8cb284
Pradhul <pradhuldev.1990@gmail.com>
2025-11-19 15:26:54 +0530

    basic list item with add button

---
+++ src/app/components/quick-list-item/quick-list-item.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IonItem, IonLabel, IonButton, IonIcon } from "@ionic/angular/standalone";
+import { addIcons } from 'ionicons';
+import { addOutline } from 'ionicons/icons';
+
+@Component({
+  selector: 'app-quick-list-item',
+  templateUrl: './quick-list-item.component.html',
+  styleUrls: ['./quick-list-item.component.scss'],
+  imports: [IonIcon, IonLabel, IonItem, IonButton],
+})
+export class QuickListItemComponent {
+
+  constructor() { 
+    addIcons({ addOutline });
+  }
+
+  
+  // Input is like the prop in react native 
+  @Input() item: string = "";
+  
+  // This is like the callback props in RN (OnPress, OnDelete..)
+  @Output() add = new EventEmitter<string>;
+  
+  onItemAdd() {
+    this.add.emit(this.item);
+  }
+
+}
