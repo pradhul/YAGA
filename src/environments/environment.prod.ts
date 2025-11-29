a9d32ee21c41ed087a48573f4535e09722fc8c1f
Pradhul <pradhuldev.1990@gmail.com>
2025-11-29 07:12:37 +0530

    added firebase database URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   firebaseConfig: {
     apiKey: "AIzaSyAAcaMbSuEAMh59KNy0UXWwzVAzVAiAz1c",
     authDomain: "yaga-b3f72.firebaseapp.com",
+    databaseURL: "https://yaga-b3f72-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "yaga-b3f72",
     storageBucket: "yaga-b3f72.firebasestorage.app",
     messagingSenderId: "975452111094",
